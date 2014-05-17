d63eac58c405d185bfa2a73dcb4bc5966fee7168
lingeast <yidongl1@uci.edu>
2014-05-17 16:00:15 -0700

    Updating test script

--- project/transaction/cmpl-run.sh
+++ project/transaction/cmpl-run.sh
@@ -3,8 +3,10 @@
 make clean
 make server
 make client
+rmiregistry -J-classpath -J.. 4444 &
 
 cd ../test.part1
 export CLASSPATH=".:gnujaxp.jar"
 javac RunTests.java
 java -ea -DrmiPort=4444 RunTests MASTER.xml
+pidof rmiregistry
